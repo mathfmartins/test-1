02cff8167fa4dcf827ef23d02c1fbd7d4608ebea
Matheus Martins <math.fmartins42@gmail.com>
2021-12-23 18:44:01 -0300

    chaves não devem ter seus valores duplicados, testando 50 valores gerados

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,10 +1,18 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
-it (`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generatedUniqueIdWithPrefix('app');
-    //expectativa
     expect(id.startsWith('app-')).toBeTrue();
-  })
-})
+  });
+
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should not generated duplicate IDs when called multiple times`, () => {
+    const service = new UniqueIdService();
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generatedUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+});
