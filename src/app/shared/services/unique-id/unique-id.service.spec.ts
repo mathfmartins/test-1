25795172e24550c15504f27760399eab811364d7
Matheus Martins <math.fmartins42@gmail.com>
2021-12-23 19:31:23 -0300

    beforeEach - uma nova UniqueIdService é instanciada a cada it executado

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,18 +1,31 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+
+  let service: UniqueIdService = null;
+  beforeEach(() => {
+    service = new UniqueIdService();
+  })
+
   it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when called with prefix`, () => {
-    const service = new UniqueIdService();
     const id = service.generatedUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
   it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should not generated duplicate IDs when called multiple times`, () => {
-    const service = new UniqueIdService();
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.generatedUniqueIdWithPrefix('app'));
     }
     expect(ids.size).toBe(50);
   });
+
+  it(`#${UniqueIdService.prototype.numberOfGeneratedUniqueIds.name} should return the number of generated IDs when called `, () => {
+    service.generatedUniqueIdWithPrefix('app');
+    service.generatedUniqueIdWithPrefix('app');
+    expect(service.numberOfGeneratedUniqueIds()).toBe(2);
+  })
 });
+
+
+
