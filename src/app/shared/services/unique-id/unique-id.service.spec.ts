3c34259dd5401eae56b2158b4fc63f0f03ac1dc5
Matheus Martins <math.fmartins42@gmail.com>
2021-12-23 17:49:18 -0300

    testando arquivo de teste, .spec

---
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -0,0 +1,9 @@
+import { UniqueIdService } from './unique-id.service';
+describe('UniqueIdService', () => {
+  it ('#generatedUniqueIdWithPrefix should generated id when with prefix', () => {
+    const service = new UniqueIdService();
+    const id = service.generatedUniqueIdWithPrefix('app');
+    //expectativa
+    expect(id).toContain('app-');
+  })
+})
