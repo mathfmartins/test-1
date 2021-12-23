ca0370d4da93063c9bbe1537d43add37e4834a02
Matheus Martins <math.fmartins42@gmail.com>
2021-12-23 18:01:26 -0300

    refatoração, UniqueIdService.prototype.generatedUniqueIdWithPrefix.name

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,7 +1,7 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
-  it ('#generatedUniqueIdWithPrefix should generated id when with prefix', () => {
+it (`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generatedUniqueIdWithPrefix('app');
     //expectativa
