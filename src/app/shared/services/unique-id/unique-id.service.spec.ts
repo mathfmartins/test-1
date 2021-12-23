65685078014af5b68a4fe67bb72f0f4e964bd976
Matheus Martins <math.fmartins42@gmail.com>
2021-12-23 18:25:48 -0300

    correção do nome da expectation

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,7 +1,7 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
-it (`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when with prefix`, () => {
+it (`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generatedUniqueIdWithPrefix('app');
     //expectativa
