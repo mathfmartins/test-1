f103dcf267f6db1c3cfc59d06f02e3af296a1954
Matheus Martins <math.fmartins42@gmail.com>
2021-12-23 18:22:40 -0300

    expect(...).toBeTrue()

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,6 +5,6 @@ it (`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should gener
     const service = new UniqueIdService();
     const id = service.generatedUniqueIdWithPrefix('app');
     //expectativa
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
   })
 })
