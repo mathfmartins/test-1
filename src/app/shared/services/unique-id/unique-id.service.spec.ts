a04181e081f6aa68fe204a2636fae38607594d80
Matheus Martins <math.fmartins42@gmail.com>
2021-12-24 09:25:01 -0300

    testando exceções

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,17 +1,21 @@
 import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
+
   let service: UniqueIdService = null;
+
   beforeEach(() => {
     service = new UniqueIdService();
   });
 
-  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should generated id when called with prefix`, () => {
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name}
+    should generated id when called with prefix`, () => {
     const id = service.generatedUniqueIdWithPrefix('app');
     expect(id.startsWith('app-')).toBeTrue();
   });
 
-  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should not generated duplicate IDs when called multiple times`, () => {
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name}
+    should not generated duplicate IDs when called multiple times`, () => {
     const ids = new Set();
     for (let i = 0; i < 50; i++) {
       ids.add(service.generatedUniqueIdWithPrefix('app'));
@@ -19,13 +23,19 @@ describe(UniqueIdService.name, () => {
     expect(ids.size).toBe(50);
   });
 
-  it(`#${UniqueIdService.prototype.numberOfGeneratedUniqueIds.name} should return the number of generated IDs when called `, () => {
+  it(`#${UniqueIdService.prototype.numberOfGeneratedUniqueIds.name}
+    should return the number of generated IDs when called `, () => {
     service.generatedUniqueIdWithPrefix('app');
     service.generatedUniqueIdWithPrefix('app');
     expect(service.numberOfGeneratedUniqueIds()).toBe(2);
   });
 
-  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name} should throw exception when called with empty`, () => {
-    expect(() => service.generatedUniqueIdWithPrefix(null)).toThrow();
+  it(`#${UniqueIdService.prototype.generatedUniqueIdWithPrefix.name}
+    should throw exception when called with empty`, () => {
+    const emptyValues = [null, undefined, '', '0', '1']
+    emptyValues.forEach(element => {
+      expect(() => service.generatedUniqueIdWithPrefix(element)).toThrow();
+    });
+
   });
 });
--- src/app/shared/services/unique-id/unique-id.service.ts
+++ src/app/shared/services/unique-id/unique-id.service.ts
@@ -4,9 +4,10 @@ import { v4 as uuidv4 } from 'uuid';
 @Injectable()
 export class UniqueIdService {
   private numberOfGenerateIds = 0;
+  private validId = /^[A-Za-z]+[\w\-\:\.]*$/;
 
   public generatedUniqueIdWithPrefix(prefix: string): string {
-    if (!prefix)
+    if (!prefix || !this.validId.test(prefix))
       throw Error('Prefix can not empty'); //fail fast
 
     const uniqueId = this.generateUniqueId();
