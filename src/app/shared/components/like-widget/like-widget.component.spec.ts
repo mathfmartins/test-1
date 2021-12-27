5edf82c3bc037e8c70e5db0791bbe5c32227b8cf
Matheus Martins <math.fmartins42@gmail.com>
2021-12-27 17:55:26 -0300

    TestBed e instâncias de componentes

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,25 +1,24 @@
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { UniqueIdService } from './../../services/unique-id/unique-id.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LikeWidgetComponent } from './like-widget.component';
 
-describe('LikeWidgetComponent', () => {
-  let component: LikeWidgetComponent;
-  let fixture: ComponentFixture<LikeWidgetComponent>;
+describe(LikeWidgetComponent.name, () => {
+    let fixture: ComponentFixture<LikeWidgetComponent> = null;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LikeWidgetComponent ]
+    beforeEach(async () => {
+      await TestBed.configureTestingModule({
+        declarations: [LikeWidgetComponent],
+        providers: [UniqueIdService],
+        imports: [FontAwesomeModule]
+      }).compileComponents();
+
+      fixture = TestBed.createComponent(LikeWidgetComponent);
+    });
+
+    it('Should create component', () => {
+      let instance = fixture.componentInstance;
+      expect(instance).toBeTruthy();
     })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LikeWidgetComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });
