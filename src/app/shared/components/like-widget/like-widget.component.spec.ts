65242fa165e756706b3893c3cb2b886f212becc6
Matheus Martins <math.fmartins42@gmail.com>
2021-12-27 18:07:43 -0300

    abordagem mais conveniente quando criamos o componente primeiro para depois testa-lo

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -1,3 +1,4 @@
+import { LikeWidgetModule } from './like-widget/like-widget.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { UniqueIdService } from './../../services/unique-id/unique-id.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
@@ -9,9 +10,13 @@ describe(LikeWidgetComponent.name, () => {
 
     beforeEach(async () => {
       await TestBed.configureTestingModule({
-        declarations: [LikeWidgetComponent],
-        providers: [UniqueIdService],
-        imports: [FontAwesomeModule]
+        // Abordagem Test First
+        // declarations: [LikeWidgetComponent],
+        // providers: [UniqueIdService],
+        // imports: [FontAwesomeModule]
+
+        // Abordagem mais conveniente quando criamos o componente primeiro e depois testamos.
+        imports: [LikeWidgetModule]
       }).compileComponents();
 
       fixture = TestBed.createComponent(LikeWidgetComponent);
