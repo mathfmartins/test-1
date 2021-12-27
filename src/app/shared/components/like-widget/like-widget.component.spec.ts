269ce67f83c6f74acca9e1b4c32d9a448fa2b9b1
Matheus Martins <math.fmartins42@gmail.com>
2021-12-27 19:06:22 -0300

    testes de geração de id

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -6,24 +6,37 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LikeWidgetComponent } from './like-widget.component';
 
 describe(LikeWidgetComponent.name, () => {
-    let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let fixture: ComponentFixture<LikeWidgetComponent> = null;
+  let component: LikeWidgetComponent = null;
 
-    beforeEach(async () => {
-      await TestBed.configureTestingModule({
-        // Abordagem Test First
-        // declarations: [LikeWidgetComponent],
-        // providers: [UniqueIdService],
-        // imports: [FontAwesomeModule]
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      // Abordagem Test First
+      // declarations: [LikeWidgetComponent],
+      // providers: [UniqueIdService],
+      // imports: [FontAwesomeModule]
 
-        // Abordagem mais conveniente quando criamos o componente primeiro e depois testamos.
-        imports: [LikeWidgetModule]
-      }).compileComponents();
+      // Abordagem mais conveniente quando criamos o componente primeiro e depois testamos.
+      imports: [LikeWidgetModule],
+    }).compileComponents();
 
-      fixture = TestBed.createComponent(LikeWidgetComponent);
-    });
+    fixture = TestBed.createComponent(LikeWidgetComponent);
+    component = fixture.componentInstance;
+  });
 
-    it('Should create component', () => {
-      let instance = fixture.componentInstance;
-      expect(instance).toBeTruthy();
-    })
+  it('Should create component', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('Should auto generate ID when id input property is missing ', () => {
+    fixture.detectChanges();
+    expect(component.id).toBeTruthy();
+  });
+
+  it('Should NOT auto generate ID when id input property is present', () => {
+    const someId = 'someId';
+    component.id = someId;
+    fixture.detectChanges();
+    expect(component.id).toBe(someId);
+  })
 });
