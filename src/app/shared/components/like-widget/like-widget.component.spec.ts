8d41245881660d287b30af0bbc4b84aa5a3f9767
Matheus Martins <math.fmartins42@gmail.com>
2021-12-28 11:30:50 -0300

    spy e toHaveBeenCalled

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -40,12 +40,10 @@ describe(LikeWidgetComponent.name, () => {
     expect(component.id).toBe(someId);
   });
 
-  it(`#${LikeWidgetComponent.prototype.like.name} Should trigger emission when called`, done => {
+  it(`#${LikeWidgetComponent.prototype.like.name} Should trigger emission when called`, () => {
+    spyOn(component.liked, 'emit');
     fixture.detectChanges();
-    component.liked.subscribe(() => {
-      expect(true).toBeTrue();
-      done()
-    })
     component.like();
+    expect(component.liked.emit).toHaveBeenCalled();
   })
 });
